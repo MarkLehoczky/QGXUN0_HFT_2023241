ce8556e68fcdcf6d04db6148206d693fb31f8d08
Mark <marklehoczky@gmail.com>
2023-10-21 20:25:54 +0200

    Repository templates added

---
+++ QGXUN0_HFT_2023241.Repository/Template/IRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace QGXUN0_HFT_2023241.Repository.Template
+{
+    /// <summary>
+    /// Repository for the <typeparamref name="T"/> model <see langword="class"/>
+    /// </summary>
+    /// <typeparam name="T">model <see langword="class"/></typeparam>
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Creates a new <paramref name="element"/> into the database context
+        /// </summary>
+        /// <param name="element">new element</param>
+        void Create(T element);
+
+        /// <summary>
+        /// Reads an <see langword="element"/> by the <paramref name="id"/> from the database context
+        /// </summary>
+        /// <param name="id">id of the readable <see langword="element"/></param>
+        /// <returns>element with the <paramref name="id"/></returns>
+        T Read(int id);
+
+        /// <summary>
+        /// Updates the <paramref name="element"/> with the same <see langword="id"/> in the database context
+        /// </summary>
+        /// <param name="element">new element with the same <see langword="id"/> as the old element</param>
+        void Update(T element);
+
+        /// <summary>
+        /// Deletes an <see langword="element"/> by the <paramref name="id"/> from the database context
+        /// </summary>
+        /// <param name="id">id of the deletable <see langword="element"/></param>
+        void Delete(int id);
+
+        /// <summary>
+        /// Reads all <see langword="element"/> from the database context
+        /// </summary>
+        /// <returns>all <see langword="element"/></returns>
+        IQueryable<T> ReadAll();
+    }
+}
---
+++ QGXUN0_HFT_2023241.Repository/Template/Repository.cs
@@ -0,0 +1,54 @@
+﻿using QGXUN0_HFT_2023241.Repository.Database;
+using System.Linq;
+
+namespace QGXUN0_HFT_2023241.Repository.Template
+{
+    /// <summary>
+    /// Repository for the <typeparamref name="T"/> model <see langword="class"/>
+    /// </summary>
+    /// <typeparam name="T">model <see langword="class"/></typeparam>
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Database context
+        /// </summary>
+        protected BookDbContext context;
+
+
+        /// <summary>
+        /// Constructor that provides the database context
+        /// </summary>
+        /// <param name="context">database context</param>
+        public Repository(BookDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        /// <inheritdoc/>
+        public void Create(T element)
+        {
+            context.Set<T>().Add(element);
+            context.SaveChanges();
+        }
+
+        /// <inheritdoc/>
+        public void Delete(int id)
+        {
+            context.Set<T>().Remove(Read(id));
+            context.SaveChanges();
+        }
+
+        /// <inheritdoc/>
+        public IQueryable<T> ReadAll()
+        {
+            return context.Set<T>();
+        }
+
+        /// <inheritdoc/>
+        public abstract T Read(int id);
+
+        /// <inheritdoc/>
+        public abstract void Update(T element);
+    }
+}
